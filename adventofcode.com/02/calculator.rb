73a8a4e54a22a470f79ee4e06aecec0b2dd0bcb6
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2015-12-02 19:56:37 +0100

    add 02 part two

--- adventofcode.com/02/calculator.rb
+++ adventofcode.com/02/calculator.rb
@@ -3,4 +3,9 @@ class WrappingPaperCalculator
     sides = [length*width, width*height, height*length]
     sides.map {|s| 2*s }.inject(&:+) + sides.sort.first
   end
+
+  def self.ribbon_required(length, width, height)
+    perimeters = [(length+width)*2, (width+height)*2, (height+length)*2]
+    perimeters.sort.first + length*width*height
+  end
 end
--- adventofcode.com/02/test_calculator.rb
+++ adventofcode.com/02/test_calculator.rb
@@ -6,4 +6,9 @@ class TestWrappingPaperCalculatorBean < MiniTest::Unit::TestCase
     assert_equal 58, WrappingPaperCalculator.wrapping_paper_required(2, 3, 4)
     assert_equal 43, WrappingPaperCalculator.wrapping_paper_required(1, 1, 10)
   end
+
+  def test_ribbon_calculation
+    assert_equal 34, WrappingPaperCalculator.ribbon_required(2, 3, 4)
+    assert_equal 14, WrappingPaperCalculator.ribbon_required(1, 1, 10)
+  end
 end
